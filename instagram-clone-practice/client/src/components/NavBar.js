d391106e2b5dfbc918ff0d915bcd9d0ce93939b9
Roberto Camberos <42880184+rcamberos23@users.noreply.github.com>
2022-11-13 13:27:11 -0800

    24 working

--- instagram-clone-practice/client/src/components/NavBar.js
+++ instagram-clone-practice/client/src/components/NavBar.js
@@ -20,7 +20,7 @@ const NavBar = () => {
   return (
     <nav>
     <div className="nav-wrapper white">
-       <Link to="/" className="brand-logo left">Instagram</Link> 
+       <Link to={state?"/":"/signin"} className="brand-logo left">Instagram</Link> 
       <ul id="nav-mobile" className="right">
         {renderList()}
       </ul>
