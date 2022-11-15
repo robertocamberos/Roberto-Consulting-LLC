89dbb2550b5376f107a874476b23de2e7ca44e7a
Roberto Camberos <42880184+rcamberos23@users.noreply.github.com>
2022-11-14 19:38:13 -0800

    server not working 2

---
+++ instagram-clone-practice/server/app.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const mongoose = require("mongoose")
+const PORT = 5000
+const {MONGOURI} = require('./keys')
+
+
+
+
+mongoose.connect(MONGOURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}) 
+mongoose.connection.on('connected', () => { 
+  console.log("Connected to mongo yeahhh")
+})
+mongoose.connection.on("error", (err) => { 
+  console.log("err connecting", err)
+})
+
+require('./models/user')
+require('./models/post')
+
+app.use(express.json())
+app.use(require('./routes/auth'))
+app.use(require('./routes/post'))
+app.use(require('./routes/user'))
+
+app.listen(PORT, () => {
+  console.log("server is running on", PORT)
+})
---
+++ instagram-clone-practice/server/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  MONGOURI: "mongodb+srv://robertocamberos:WOeLZvWgOI3tckUN@cluster0.efzfzgr.mongodb.net/?retryWrites=true&w=majority",
+  JWT_SECRET: "randomstring"
+}
